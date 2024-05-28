e0dddc0d272ac5141ec5c190b38936dec604eb8b
Konrad Thiemann <konrad.thiemann@mdm.de>
2024-05-28 14:32:29 +0200

    remove user from todo

--- todo/models.py
+++ todo/models.py
@@ -4,12 +4,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class TodoItem(models.Model):
+class Todo(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=500)
     created_at = models.DateField(default=datetime.date.today)
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        default=None
-    )
+    description = models.CharField(max_length=500)
+    # user = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     default=None,
+    # )
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .models import Todo
+from .serializers import TodoSerializer
 # Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Todo.objects.all().order_by('-created_at')
+    serializer_class = TodoSerializer
+    permission_classes = [] # permission.IsAuthenticateditem
